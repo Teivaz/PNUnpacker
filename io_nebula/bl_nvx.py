f9339a42d5569ceb419735b339364bd689ea1582
teivaz <github@teivaz.com>
2019-02-18 21:14:11 +0100

    Fixed importing UV

--- io_nebula/bl_nvx.py
+++ io_nebula/bl_nvx.py
@@ -79,24 +79,33 @@ def addMesh(filename, objName):
         stream = InputStream(f)
         mesh = Mesh(stream=stream)
 
-    bmesh = bpy.data.meshes.new(objName)
-    bmesh.from_pydata(mesh.positions, [], mesh.indices_as_triangles())
+    bl_mesh = bpy.data.meshes.new(objName)
+    bl_mesh.from_pydata(mesh.positions, [], mesh.indices_as_triangles())
     
     if mesh.normals:
-        bmesh.create_normals_split()
+        bl_mesh.create_normals_split()
         normals = [n.data() for n in mesh.normals]
 
-        for l in bmesh.loops:
+        for l in bl_mesh.loops:
             l.normal[:] = normals[l.vertex_index]
 
-        bmesh.normals_split_custom_set_from_vertices(normals)
-        bmesh.use_auto_smooth = True
+        bl_mesh.normals_split_custom_set_from_vertices(normals)
+        bl_mesh.use_auto_smooth = True
 
     def add_uv(data, name):
-        bmesh.uv_textures.new("UV0")
-        uv_layer = bmesh.uv_layers[-1]
-        for i in mesh.indices:
-            uv_layer.data[i].uv = data[i].data()
+        indices_as_triangles = mesh.indices_as_triangles()
+        bl_mesh.uv_textures.new(name)
+        bm = bmesh.new()
+        bm.from_mesh(bl_mesh)
+        uv_layer = bm.loops.layers.uv[-1]
+
+        nFaces = len(bm.faces)
+        bm.faces.ensure_lookup_table()
+        for fi in range(nFaces):
+            indices = indices_as_triangles[fi]
+            for i in range(3):
+                bm.faces[fi].loops[i][uv_layer].uv = data[indices[i]]
+        bm.to_mesh(bl_mesh)
 
     if mesh.uv0:
         add_uv(mesh.uv0, "UV0")
@@ -107,30 +116,13 @@ def addMesh(filename, objName):
     if mesh.uv3:
         add_uv(mesh.uv3, "UV3")
 
-    bmesh.update()
-    bmesh.validate()
-    """
-    hasUV = len(mesh.UV0) > 0
-    if hasUV:
-        m.uv_textures.new("UV0")
-        bm = bmesh.new()
-        bm.from_mesh(m)
-        uv_layer = bm.loops.layers.uv[0]
-
-        nFaces = len(bm.faces)
-        bm.faces.ensure_lookup_table()
-        for fi in range(nFaces):
-            indices = mesh.Indices[fi]
-            for i in range(3):
-                bm.faces[fi].loops[i][uv_layer].uv = mesh.UV0[indices[i]]
-        bm.to_mesh(m)
-    """
-
+    bl_mesh.update()
+    bl_mesh.validate()
     scn = bpy.context.scene
 
     for o in scn.objects:
         o.select = False
-    nobj = bpy.data.objects.new(objName, bmesh)
+    nobj = bpy.data.objects.new(objName, bl_mesh)
     scn.objects.link(nobj)
     nobj.select = True
 
